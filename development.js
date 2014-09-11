c162a31225d6b223f89807fe54d4015ed4b05388
davidbrock <dbrbv@allstate.com>
2014-09-11 11:06:13 -0500

    adding files

---
+++ development.js
@@ -0,0 +1,44 @@
+
+var deployd = require('deployd');
+var url = require('url');
+var port = (process.env.VCAP_APP_PORT || 3000);
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+
+var mongourl;
+
+if(process.env.VCAP_SERVICES){
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  mongourl = env['mongolab'][0]['credentials']['uri'];
+}else{
+
+  mongourl = "mongodb://:@localhost:27017/db_name";
+
+}
+var db_url = url.parse(mongourl);
+
+var server = deployd({
+    port: port,
+    env: 'development',
+    db: {
+        "host": db_url.hostname,
+        "port": parseInt(db_url.port),
+        "name": db_url.pathname.slice(1),
+        "credentials": {
+            "username": db_url.auth.split(':')[0],
+            "password": db_url.auth.split(':')[1]
+        }
+    }
+});
+
+server.listen();
+
+server.on('listening', function() {
+  console.log("Server is listening");
+});
+
+server.on('error', function(err) {
+  console.error(err);
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
+  });
+});
